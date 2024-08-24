3c258a440f43a5afff148558798b6dfcc8b830ce
Danilo <nordan@misena.edu.co>
2024-08-23 22:10:42 -0500

    Funcionalidad de evento sin sobrecargar el html

--- CrudInicial/src/app/app.component.ts
+++ CrudInicial/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { PersonasComponent } from './personas/personas.component';
+//import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
--- CrudInicial/src/app/personas/personas.component.ts
+++ CrudInicial/src/app/personas/personas.component.ts
@@ -1,21 +1,39 @@
 import { Component } from '@angular/core';
 import { PersonaComponent } from '../persona/persona.component';
-
+import { FormsModule } from '@angular/forms';
+import { NgFor } from '@angular/common';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-personas',
   standalone: true,
-  imports: [PersonaComponent],
+  imports: [PersonaComponent, FormsModule,NgFor, CommonModule],
   templateUrl: './personas.component.html',
   styleUrl: './personas.component.css'
 })
 export class PersonasComponent {
   deshabilitar = false;
   mensaje='No se ha agregado ninguna persona';
+  titulo='Prueba de testeo';
+mostrar=false;
+
 
 AgregarPersona(){
+  this.mostrar= true;
 this.mensaje='Persona Agregada';
 
-
 }
-   
+/*
+Esto debe ir en el html
+			      ----      <input type="text" class="form-control" (input)="modificarTitulo($event)">
+            
+            ----      {{titulo}}
+			
+
+
+modificarTitulo(event: Event){
+  console.log('Entrando a metodo modificar titulo');
+  //se esta capturando el valor que se recibe del input como valor de html y se le hace cast para tomarlo como dato
+this.titulo=(<HTMLInputElement>event.target).value;
+}*/
+
 }
