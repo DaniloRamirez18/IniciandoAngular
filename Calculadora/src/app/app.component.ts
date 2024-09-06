389414e1d605042ea5763fe78c45a275a7338cba
Danilo <nordan@misena.edu.co>
2024-09-05 22:57:12 -0500

    Calculadora usada con commponentes

---
+++ Calculadora/src/app/app.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, FormsModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title='Hola llego a la calculadora'
+  operandoA: number = 0;
+  operandoB: number = 0;
+  resultado: number = 0;
+  
+sumar(): void{
+this.resultado=this.operandoA + this.operandoB
+}
+
+}
+
+
+
+
